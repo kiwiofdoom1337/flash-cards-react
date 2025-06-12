dc1c798a72169ed4acde862b97dba36068bdc30b
kiwiofdoom <armans.a.ahmetjanovs@gmail.com>
2025-06-12 14:15:11 +0300

    Initial commit

---
+++ src/questions.js
@@ -0,0 +1,61 @@
+export const questionsAnswers = [{
+  question: 'What is the difference between var, let, and const?',
+  answer: 'In JavaScript, var is function-scoped and can be re-declared; let and const are block-scoped with let allowing re-assignment and const preventing it. However, const objects can have their contents modified.'
+}, {
+  question: 'How was your day?',
+  answer: 'It was good, thank you!'
+}, {
+  question: 'How many times have you eaten bread in your life?',
+  answer: 'How many time have you breathed in and out?'
+}, {
+  question: 'What do you think about birds?',
+  answer: 'Birds are cool'
+}, {
+  question: 'Do you like jumping?',
+  answer: 'Jumping jacks!'
+}, {
+  question: 'If I had 10 apples and Marty took 5, how many apples do I have?',
+  answer: 'I have 5 apples!'
+}, {
+  question: 'How heavy is a kilogram of feathers?',
+  answer: 'Same weight as a kilogram of steel'
+}, {
+  question: 'How many questions does this page have?',
+  answer: 'I hope not too many'
+}, {
+  question: "What's the big deal about yellow baloons?",
+  answer: 'They are yellow and they are baloons'
+}, {
+  question: 'If you know something, would you tell it?',
+  answer: 'Depends on something'
+}, {
+  question: 'How old is the oldest television?',
+  answer: 'oldestTv.age'
+}, {
+  question: 'Do you think this dress is making me look fat?',
+  answer: '...'
+}, {
+  question: 'Cats or dogs?',
+  answer: 'Why not both?'
+}, {
+  question: 'Do you like baseball?',
+  answer: 'Only in Japan'
+}, {
+  question: 'How much sleep do you need?',
+  answer: 'More than I have right now'
+}, {
+  question: 'What is the angle of a 90 degree angle?',
+  answer: 'Depends on a 90 degree angle'
+}, {
+  question: 'What is stronger, brick house or a cement house?',
+  answer: 'Next question please'
+}, {
+  question: 'Do you get wetter if you walk or run under the rain?',
+  answer: 'What kind of rain?'
+}, {
+  question: 'Are wi-fi signal waves visible?',
+  answer: "Only when wi-fi does'nt work"
+}, {
+  question: 'How do you like your steak?',
+  answer: 'Well done'
+}]
